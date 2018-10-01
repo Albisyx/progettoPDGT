aa21c1ca4b3300560d0f8a8de4a1f81497af4895
Albisyx <a.spadoni9@campus.uniurb.it>
2018-10-01 12:30:34 +0200

    Aggiunta autenticazione con i server di spotify
    Serve per ottenere un token che permette di effettiare chiamate alle ApI di spotify

--- API/server.js
+++ API/server.js
@@ -1,10 +1,37 @@
-var express = require('express');
-var rp = require('request-promise');
-var app = express();
+let express = require('express');
+let rp = require('request-promise');
+let SpotifyWebApi = require('spotify-web-api-node');
+let credenziali = require('./credenziali.js'); // file contenente le credenziali per autenticarsi con spotify
+                                               // esso non è presente nella repository
+let app = express();
+
+// uso la libreria spotify-web-api-node per ottenere un acces_token
+// uso la modalità di autenticazione fornita da spotify che permette di ottenere, 
+// da parte di un client, un token per poter eseguire le API
+// quindi l'autenticazione è a livello di client, non a livello di utente
+
+let spotifyApi = new SpotifyWebApi({
+  clientId: credenziali.client_id, 
+  clientSecret: credenziali.client_secret
+});
+
+// invoco la funzione per ottenere il token
+spotifyApi.clientCredentialsGrant()
+  .then(function(data) 
+  {
+      console.log('Token ' + data.body['access_token']);
+
+      // salvo il token almeno lo posso usare nelle future chiamate
+      spotifyApi.setAccessToken(data.body['access_token']);
+  },
+  function(err) 
+  {
+      console.log(err);
+  });
 
 app.get('/', (req, res) => 
 {
-    res.send('<h1>homepage</h1>');
+    res.send('homepage').end();
 });  
     
 app.listen(3000, function()
