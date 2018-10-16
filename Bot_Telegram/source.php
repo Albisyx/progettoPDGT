7f64c9c9a8e8175510770d2a86f0c76eb69ab929
Albisyx <a.spadoni9@campus.uniurb.it>
2018-10-16 12:51:44 +0200

    Funzioni per interagire col database spostate

--- Bot_Telegram/source.php
+++ Bot_Telegram/source.php
@@ -90,7 +90,7 @@
 			return false;
 		}
 	}
-	
+
 	// metodo che si interfaccia al percorso dell'API ?/new-releases
 	function getNewReleases()
 	{
@@ -113,26 +113,5 @@
 		}
 
 		send($GLOBALS['cid'], $nuoveUscite);
-	}
-	
-	function update_state($cid, $state)
-	{
-		$dati_utente_db = mysql_query("SELECT * FROM comando_eseguito WHERE cid = '$cid'");
-		$array = mysql_fetch_array($dati_utente_db);
-
-		if($array[cid] == $cid)
-			mysql_query("UPDATE comando_eseguito SET comando = '$state' WHERE cid = '$cid'");
-		else
-			mysql_query("INSERT INTO comando_eseguito (cid, comando) VALUES ('$cid', '$state')");
-	}
-
-	function getState($cid)
-	{
-		$risultato = mysql_query("SELECT comando FROM comando_eseguito WHERE cid='$cid'");
-		$row = mysql_fetch_assoc($risultato);
-		
-		$stato = $row['comando'];
-		
-		return $stato;
 	}
 ?>
