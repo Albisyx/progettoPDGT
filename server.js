b77edd98914683034d67fd3560c720904115ca53
Albisyx <a.spadoni9@campus.uniurb.it>
2018-10-18 16:03:28 +0200

    corezione errore di distrazione

--- server.js
+++ server.js
@@ -220,7 +220,7 @@ function getArtistFromTrack(trackName, res)
     rp(trackOptions)
       .then(function(data)
       {
-          if(data['tracks']['total'] == 0)
+          if(data['tracks']['total'] != 0)
               getLyrics(data['tracks']['items'][0]['artists']['name'], encodeURIComponent(trackName), res);
           else
           	  res.status(404).send({error : 'Artista non trovato partendo da questa canzone'});
